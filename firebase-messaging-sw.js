5492ab4b7f568fbf8a761e2bc5ad386df9d703a7
wanwan89 <iwansetwan26@gmail.com>
2026-04-07 23:18:24 +0700

    Update rutin: 07-04-2026 23:18:24

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-// FIREBASE SERVICE WORKER (FIX EVALUATION)
+// FIREBASE SERVICE WORKER (FXIX EVALUATION)
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
